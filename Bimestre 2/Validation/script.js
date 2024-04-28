2092071d60efa7eb2ccdbe6efcbd7c1012ef2fc3
ViniciusAP13 <viniciusamarop@gmail.com>
2024-04-27 21:20:52 -0300

    Baboseiras feitas

--- Bimestre 2/Validation/script.js
+++ Bimestre 2/Validation/script.js
@@ -1,9 +1,12 @@
 function autenticar() {
     let usuario = document.getElementById("login").value;
     let senha = document.getElementById("senha").value;
+    let form = document.getElementById("formulario");
     if (usuario=== "usuario" && senha=== "123") {
         document.getElementById("mensagem").innerHTML="Usuário autenticado.";
+        form.style.backgroundColor = "rgb(0, 255, 0)";
     } else {
         document.getElementById("mensagem").innerHTML="Autenticação inválida.";
+        form.style.backgroundColor = "rgb(255, 0, 0)";
     }
 }
